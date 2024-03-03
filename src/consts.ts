fd5c78888a8ff0f49d24754dcdddb8f80078336f
Kirikaze Chiyuki <me@chyk.ink>
2024-03-03 21:01:20 +0800

    🔧 初回 commit

---
+++ src/consts.ts
@@ -0,0 +1,15 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const THEME_VERSION = '0.0.1'
+
+export const SITE_TITLE = '千雪的咖啡厅'
+export const SITE_DESCRIPTION = '浩繁星空下的一场稚嫩的梦'
+
+export const AVATAR_URL = 'https://q1.qlogo.cn/g?b=qq&nk=3526514925&s=640'
+
+export const CDN_PREFETCH_DOMAINS = [
+    'https://gcore.jsdelivr.net',
+    'https://cdn.bootcdn.net',
+    'https://ydz-cos.zyglq.cn'
+]
